7085a040fb6b776e61b16b3eb69da0b883745149
sambenson <sam@futurebuilding.co.uk>
2016-02-07 20:56:52 +0000

    Added analytics

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,6 +3,14 @@ chrome.extension.sendMessage({}, function(response) {
 		if (document.readyState === "complete") {
 			clearInterval(readyStateCheckInterval);
 
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-73237610-1', 'auto');
+			ga('send', 'pageview');
+
 			(function($){
 			    var myCurrency = 'GBP',
 			        codersClanFee = 0.3;
